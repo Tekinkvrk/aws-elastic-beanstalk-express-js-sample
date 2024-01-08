1386bd189b59f75a90fb5f8c37f60c53e04d1ef1
Tekinkvrk <tknkvrk@gmail.com>
2024-01-08 17:38:15 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Tekin Kıvrak-Solutıons Architect goal'));
+app.get('/', (req, res) => res.send('Tekin Kıvrak-Solutıons Architect goal24'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
